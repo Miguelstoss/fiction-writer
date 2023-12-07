ed7b7a02011e38592b6376dac28ebd1d52652c25
Miguel de Sousa Santos <miguelstoss@gmail.com>
2023-12-06 21:52:54 -0300

    Aula 01 - React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Editor from './components/Editor';
+import Header from './components/Header';
+import Stories from './components/Stories';
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Stories />
+      <Editor />
+    </>
+  );
+}
---
+++ src/components/Stories.jsx
@@ -0,0 +1,9 @@
+import './Stories.css';
+
+export default function Stories() {
+    return (
+        <div className="stories">
+            <h2>Histórias</h2>
+        </div>
+    );
+}
